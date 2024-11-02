93b1fd6ae32583d64a8e873e93ec7b59838d86d0
peurKe <emmanuel.houriez@gmail.com>
2024-11-02 17:16:32 +0100

    Update script description

--- auto_ZONA_translator.py
+++ auto_ZONA_translator.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Title : auto_ZONA_translator.py
-Description : Script for translating all Ukrainian or Russian texts of the Steam games 'Z.O.N.A Origin' and 'Z.O.N.A Project X' by AGaming+ and enjoy Russian voices!
+Description : Script for translating all Ukrainian or Russian texts of the Steam games 'Z.O.N.A Origin' and 'Z.O.N.A Project X' by AGaming+ and enjoy Russian voices while having all the texts in your native language!
 Author: peurKe
 Creation Date: 2024-10-31
 Last Modified: 2024-10-31
